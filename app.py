1c08ff125cd367f76138cbd61bb1c898f96c4a53
csalerno1 <77583966+csalerno1@users.noreply.github.com>
2022-11-25 23:08:00 -0500

    commit

--- app.py
+++ app.py
@@ -13,22 +13,92 @@ def home():
 def hello_there(name=None):
         return render_template(
         "EggBites2.html",
-        name=name,
-        date=datetime.now()
     )
 
 @app.route("/ChickenCacciatore")
 def hello_there(name=None):
         return render_template(
         "ChickenCacciatore2.html",
-        name=name,
-        date=datetime.now()
     )
 
 @app.route("/CreamyChickenCasserole")
 def hello_there(name=None):
         return render_template(
-        "EggBites2.html",
+        "CreamyChickenCasserole2.html",
+    )
+
+
+@app.route("/About")
+def hello_there(name=None):
+        return render_template(
+        "about.html",
+    )
+
+
+@app.route("/Contact")
+def hello_there(name=None):
+        return render_template(
+        "contact.html"
+    )
+
+@app.route("/DevilsFoodCakeCookies")
+def hello_there(name=None):
+        return render_template(
+        "DevilsFoodCakeCookies2.html"
+    )
+
+@app.route("/GuinnessBeefStew")
+def hello_there(name=None):
+        return render_template(
+        "GuinnessBeefStew2.html"
+    )
+
+@app.route("/Index")
+def hello_there(name=None):
+        return render_template(
+        "index.html"
+    )
+
+@app.route("/Index/<name>")
+def hello_there(name=None):
+        return render_template(
+        "index.html",
         name=name,
         date=datetime.now()
+    )
+
+@app.route("/ProjectDetails")
+def hello_there(name=None):
+        return render_template(
+        "project-details.html"
+    )
+
+@app.route("/Project")
+def hello_there(name=None):
+        return render_template(
+        "project.html"
+    )
+
+@app.route("/RecipesSidebar")
+def hello_there(name=None):
+        return render_template(
+        "recipes-sidebar.html"
+    )
+
+@app.route("/RecipesSidebar2")
+def hello_there(name=None):
+        return render_template(
+        "recipes-sidebar2.html"
+    )
+
+@app.route("/UnstuffedCabbage")
+def hello_there(name=None):
+        return render_template(
+        "UnstuffedCabbage2.html"
+    )
+
+@app.route("/ZucchiniBread")
+def hello_there(name=None):
+        return render_template(
+        "ZucchiniBread2.html"
     )
